f97badcfcb7e614473aa307c670c0dc9eeb9c944
eheine <enrico.heine@immobilienscout24.de>
2015-10-01 15:09:57 +0200

    use python 2 and 3 compatible lower function

--- src/main/python/aws_federation_proxy/provider/grp_provider.py
+++ src/main/python/aws_federation_proxy/provider/grp_provider.py
@@ -3,10 +3,11 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 from aws_federation_proxy.provider import ProviderByGroups
 
 import grp
+from six import text_type
 
 
 class Provider(ProviderByGroups):
     """Uses the builtin grp module to retrieve group information"""
 
     def get_group_list(self):
-        return [g.gr_name for g in grp.getgrall() if self.user.lower() in map(unicode.lower, g.gr_mem)]
+        return [g.gr_name for g in grp.getgrall() if self.user.lower() in map(text_type.lower, g.gr_mem)]
