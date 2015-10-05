3a993429c58f21180ed18ff0944f13ef5d90af46
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2015-10-05 18:28:19 +0200

    Fix unicode f** in grp module

--- src/main/python/aws_federation_proxy/provider/grp_provider.py
+++ src/main/python/aws_federation_proxy/provider/grp_provider.py
@@ -3,11 +3,10 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 from aws_federation_proxy.provider import ProviderByGroups
 
 import grp
-from six import text_type
 
 
 class Provider(ProviderByGroups):
     """Uses the builtin grp module to retrieve group information"""
 
     def get_group_list(self):
-        return [g.gr_name for g in grp.getgrall() if self.user.lower() in map(text_type.lower, g.gr_mem)]
+        return [g.gr_name for g in grp.getgrall() if self.user.lower() in map(str.lower, g.gr_mem)]
