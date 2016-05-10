0c6010f9fdf11f7354403f3484a72af46d52d340
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-05-10 18:27:39 +0200

    Depend on boto3 instead of boto2

--- build.py
+++ build.py
@@ -36,7 +36,7 @@ def set_properties(project):
     project.depends_on("six")
     project.depends_on("yamlreader")
     project.depends_on("bottle")
-    project.depends_on("boto>=2.38.0")
+    project.depends_on("boto3")
 
     project.set_property("verbose", True)
     project.set_property('flake8_include_test_sources', True)
