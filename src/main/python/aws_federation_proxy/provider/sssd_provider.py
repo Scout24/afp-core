35588acf6ece4d4e05ef839fd7632708e32c76f1
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2017-05-10 15:58:31 +0200

    Delay import of pysss as much as possible
    When importing at the top level, it makes the build fail (build server does
    not have the module) with pybuilder 0.11.9 even though the module is not even
    tested. Pybuilder 0.11.9 seems to be the last version that still works with
    our Python 2.6 setup, so we cannot update to 0.11.10 which does not have the
    problem.

--- src/main/python/aws_federation_proxy/provider/sssd_provider.py
+++ src/main/python/aws_federation_proxy/provider/sssd_provider.py
@@ -3,11 +3,10 @@
 
 from aws_federation_proxy.provider import ProviderByGroups
 
-import pysss
-
 
 class Provider(ProviderByGroups):
     """Uses the pysss module to retrieve group information from SSSD"""
 
     def get_group_list(self):
+        import pysss
         return pysss.getgrouplist(self.user)
